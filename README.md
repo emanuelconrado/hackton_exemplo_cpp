ac104e22a46701958cee564b15526373c39311d4
Duel <emanuelthadeu45@gmail.com>
2025-01-22 21:59:16 -0300

    print is_on_

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Exemplo de como deve ser o funcionamento do arm e do take of
+# Exemplo de como deve ser o funcionamento do arm e do take of
--- main.cpp
+++ main.cpp
@@ -16,13 +16,13 @@ void arm(){
     std::cout << "moteres girando..." << std::endl;
     seconds timer_(5);
     auto inicio = steady_clock::now();
+    is_on_ = true;
 
     while (duration_cast<seconds>(steady_clock::now() - inicio) < timer_)
     {
         std::cout << "girando a 15rpm" << std::endl;
-        is_on_ = true;
 
-        if(takeof_on_){
+        if(!is_on_){
             std::cout << "takeof ativado" << std::endl;
             return;
         }
@@ -37,35 +37,34 @@ void going_to(std::vector<int> gate){
     std::cout << "Gate: " << "'" << gate[0] << ", " << gate[1] << ", "<< gate[2] << "'"<< std::endl;
     std::this_thread::sleep_for(std::chrono::seconds(3));
 
-    if(actual_gate == 3){
+    if(actual_gate == 4){
         std::this_thread::sleep_for(std::chrono::seconds(7));
     }
-    is_on_ = true;
+    is_on_ = false;
 }
 
 void take_of(){
     std::this_thread::sleep_for(std::chrono::seconds(3));
-    takeof_on_ = true;
-    if(is_on_){
+    is_on_ = false;
+    if(!is_on_){
         std::cout << "levantando o drone" << std::endl;
         while (true){
             std::thread t_going_to(going_to, gates[actual_gate]);
             t_going_to.detach();
-            is_on_ = false;
+            is_on_ = true;
 
             seconds timer_(5);
             auto inicio = steady_clock::now();
 
             while (duration_cast<seconds>(steady_clock::now() - inicio) < timer_){
-                //std::cout << "esperando estado anterior" << std::endl;
-
-                if(is_on_){
+                if(!is_on_){
                     std::cout << "estado alcançado" << std::endl;
+                    //std::this_thread::sleep_for(std::chrono::seconds(3));
                     break;
                 }
             }
 
-            if(!is_on_){
+            if(is_on_){
                 std::cout << "erro ao ir para gate: " << "'" << gates[actual_gate][0] << ", " << gates[actual_gate][1] << ", "<< gates[actual_gate][2] << "'"<< std::endl;
                 break;
             }
@@ -86,12 +85,25 @@ void take_of(){
     std::cout << "arm não inicializado" << std::endl;
 }
 
+void print(){
+    seconds timer_(20);
+    auto inicio = steady_clock::now();
+
+    while (duration_cast<seconds>(steady_clock::now() - inicio) < timer_)
+    {
+        std::cout << std::boolalpha;
+        std::cout << "is_on_: " << is_on_ << std::endl;
+    }
+}
+
 int main(){
     std::thread t_arm(arm);
     std::thread t_take_of(take_of);
+    //std::thread t_print(print);
 
     t_take_of.join();
     t_arm.join();
+    //t_print.join();
 
     return 0;
 }
